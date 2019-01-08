cd0d88f423fdf7293ec0eac7e796e3140aeefa43
Zaman3027 <zamanapp@outlook.com>
2019-01-09 05:10:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+var admin = require("firebase-admin");
+var db = admin.initializeApp();
+db = db.database()
+var ref = db.ref("/User/");
+
+ exports.onMessageCreate = functions.database.ref('/User/{Userid}')
+ .onCreate((snapshot,context)=>{
+     const Userid = context.params.Userid;
+     console.log(`New Userid ${Userid}`)
+     const userData = snapshot.ref.parent.on('value',data=>{
+         let keys = []
+        data.forEach(value=>{
+            keys.push(value.key)
+            if(keys.length>10){
+                data.child(keys[9]).ref.remove()
+            }
+        })
+
+        for(var i =0; i<keys.length; i++){
+            keys.pop()
+        }
+    });
+
+     console.log('Done')
+     return userData;
+
+ })
